fc7227fdc6bad88b4e46d4bffa99229d8a7df861
Sylvain Roncoroni <sylvain.ronco@gmail.com>
2021-01-25 13:13:46 +0100

    Add todo

--- DemoFreeImage/main.cpp
+++ DemoFreeImage/main.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
 #include "FreeImage.h"
 
+/*
+math
+ - Vector3d
+ - Matrix
+render
+ - Material
+ - Camera
+ - LightSource
+ - Ray
+objects
+ - Object3d
+ - Object2d ?
+ - Sphere
+ - Cube (cube et rectangle et trucs)
+ - ... formes à implémenter
+image + parser
+ - Parser
+engine
+ - Engine
+ - Scene
+
+Classes: PascalCase
+Fonctions: camelCase
+Membres: camelCase, pas de _
+*/
+
 int main(int argc, char** argv)
 {
+	
 	RGBQUAD color;
 	FIBITMAP* image;
 	image = FreeImage_Allocate(256, 256, 32);
